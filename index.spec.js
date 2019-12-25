29f87f3b0d5a48607270802b153f46e9b862f173
KIMINJOONG <ijboym216@gmail.com>
2019-12-25 09:58:45 +0900

    make a get user list API test code

--- index.spec.js
+++ index.spec.js
@@ -12,5 +12,22 @@ describe("GET /users는", () => {
           done();
         });
     });
+
+    it("최대 limit 갯수만큼 응답한다", done => {
+      request(app)
+        .get("/users?limit=2")
+        .end((err, res) => {
+          res.body.should.have.lengthOf(2);
+          done();
+        });
+    });
+  });
+  describe("실패시", () => {
+    it("숫자형이 아니면 400을 응답한다.", done => {
+      request(app)
+        .get("/users?limit=two")
+        .expect(400)
+        .end(done);
+    });
   });
 });
