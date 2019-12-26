c439199001be81fd1dd174a455a36340072e82a9
KIMINJOONG <ijboym216@gmail.com>
2019-12-26 09:40:32 +0900

    split route, api logic

--- api/user/index.js
+++ api/user/index.js
@@ -1,80 +1,16 @@
 // 라우팅 설정
 const express = require("express");
 const router = express.Router();
-let users = [
-  { id: 1, name: "alice" },
-  { id: 2, name: "bek" },
-  { id: 3, name: "chris" }
-];
-router.get("/", (req, res) => {
-  req.query.limit = req.query.limit || 10;
-  const limit = parseInt(req.query.limit, 10);
-  if (Number.isNaN(limit)) {
-    return res.status(400).end();
-  }
-  res.json(users.slice(0, limit));
-});
+const ctrl = require("./user.ctrl");
 
-router.get("/:id", (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  if (Number.isNaN(id)) {
-    return res.status(400).end();
-  }
-  const user = users.filter(user => user.id === id)[0];
-  if (!user) {
-    return res.status(404).end();
-  }
-  return res.json(user);
-});
+router.get("/", ctrl.index);
 
-router.delete("/:id", (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  if (Number.isNaN(id)) {
-    return res.status(400).end();
-  }
-  users = users.filter(user => user.id !== id);
-  return res.status(204).end();
-});
+router.get("/:id", ctrl.show);
 
-router.post("/", (req, res) => {
-  const name = req.body.name;
+router.delete("/:id", ctrl.destroy);
 
-  if (!name) {
-    return res.status(400).end();
-  }
+router.post("/", ctrl.create);
 
-  const isConflict = users.filter(user => user.name === name).length;
-  if (isConflict) {
-    return res.status(409).end();
-  }
-  const id = Date.now();
-  const user = { id, name };
-  users.push(user);
-  return res.status(201).json(user);
-});
-
-router.put("/:id", (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  if (Number.isNaN(id)) {
-    return res.status(400).end();
-  }
-
-  const name = req.body.name;
-  if (!name) {
-    return res.status(400).end();
-  }
-
-  const isConflict = users.filter(user => user.name === name).length;
-  if (isConflict) {
-    return res.status(409).end();
-  }
-
-  const user = users.filter(user => user.id === id)[0];
-  if (!user) {
-    return res.status(404).end();
-  }
-  user.name = name;
-  return res.json(user);
-});
+router.put("/:id", ctrl.update);
 
 module.exports = router;
