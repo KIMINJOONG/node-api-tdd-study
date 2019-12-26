e04ff74dc3b8539d15f2734cf1a2f656a6193964
KIMINJOONG <ijboym216@gmail.com>
2019-12-26 09:47:04 +0900

    finished to refactorying code

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const morgan = require("morgan");
 
 const user = require("./api/user");
 
-app.use(morgan("dev"));
+if (process.NODE_ENV !== "test") {
+  app.use(morgan("dev"));
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/users", user);
-
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
-});
 
 module.exports = app;
